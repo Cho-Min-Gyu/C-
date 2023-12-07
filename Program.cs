ec9d77c42475bc6949d501b4698e0f203e2c6866
Mingyu Cho <bigmingyu0070@gmail.com>
2023-12-07 22:49:32 +0900

    C# 첫번째

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// C# 위대한 시작
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World..!"); // WriteLine은 개행 메서드
+        }
+    }
+} 
